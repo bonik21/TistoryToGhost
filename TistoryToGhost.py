5e75ce06274e0b04cb586e481579642ae87c41a3
BoniK <mail@bonik.me>
2023-07-15 16:57:12 +0900

    티스토리 백업 폴더 정보 조회

--- TistoryToGhost.py
+++ TistoryToGhost.py
@@ -7,17 +7,17 @@ TISTROY_BACKUP_PATH = Tistory_Read_info.TISTROY_BACKUP_PATH
 slug_list = Tistory_Read_info.get_slug_list(TISTROY_BACKUP_PATH)
 
 
-# # 현재 고스트에 등록된 게시물 수 확인
-# all_ghost_content = Ghost_Read_with_content_api.read_all_ghost_content()
-# print(f'{API_URL}에 등록된 게시물 : {len(all_ghost_content)}개')
+# 현재 고스트에 등록된 게시물 수 확인
+all_ghost_content = Ghost_Read_with_content_api.read_all_ghost_content()
+print(f'{API_URL}에 등록된 게시물 : {len(all_ghost_content)}개')
 
 
-# # 티스토리 백업 폴더에 있는 게시물 수 확인
-# print(f'{TISTROY_BACKUP_PATH}에 게시물이 {len(slug_list)}개 있습니다.')
+# 티스토리 백업 폴더에 있는 게시물 수 확인
+print(f'{TISTROY_BACKUP_PATH}에 게시물 {len(slug_list)}개')
 
 
-# # 티스토리 백업 폴더에 있는 게시물 slug 확인
-# print(f'게시물 목록 " {slug_list}')
+# 티스토리 백업 폴더에 있는 게시물 slug 확인
+print(f'티스토리 게시물 목록 " {slug_list}')
 
 
 # # 티스토리 백업 폴더에 있는 파일 목록 확인
